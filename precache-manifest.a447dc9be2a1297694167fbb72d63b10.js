8004bd3b08cbd27ff6c3b4a890231e321e735304
arenchilingaryan <58465410+arenchilingaryan@users.noreply.github.com>
2020-02-23 23:22:25 +0300

    Updates

---
+++ precache-manifest.a447dc9be2a1297694167fbb72d63b10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7551ae723100629964b6c91ac92053",
+    "url": "/simpletools/index.html"
+  },
+  {
+    "revision": "6d5f13c46815f6d7dfba",
+    "url": "/simpletools/static/css/main.00b5fcbc.chunk.css"
+  },
+  {
+    "revision": "1b377bd8e59dd09b3a57",
+    "url": "/simpletools/static/js/2.19bad31e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simpletools/static/js/2.19bad31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5f13c46815f6d7dfba",
+    "url": "/simpletools/static/js/main.253c8753.chunk.js"
+  },
+  {
+    "revision": "a52933eb3f57b2b26b49",
+    "url": "/simpletools/static/js/runtime-main.938ac858.js"
+  }
+]);
